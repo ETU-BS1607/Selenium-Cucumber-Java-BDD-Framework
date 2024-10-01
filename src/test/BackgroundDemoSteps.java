a06f24a19c3f88fe84f3ec4f070fbb452ff105b4
ETU-BS1607 <jannatun.nur@brainstation-23.com>
2024-10-01 16:10:43 +0600

    Added Background

---
+++ src/test/BackgroundDemoSteps.java
@@ -0,0 +1,34 @@
+package Background;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BackgroundDemoSteps {
+
+
+	@When("user clicks on welcome link")
+	public void user_clicks_on_welcome_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("logout link is displayed")
+	public void logout_link_is_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@And("user clicks on dashboard link")
+	public void user_clicks_on_dashboard_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("quick launch toolbar is displayed")
+	public void quick_launch_toolbar_is_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
